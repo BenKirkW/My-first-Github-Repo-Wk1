2ba295da023f99c43ec35d9d211b6b94d881257b
Ben Wildey <benwildey8@gmail.com>
2025-01-28 11:47:33 +0000

    first commit to set up my r project

---
+++ wk_1.R
@@ -0,0 +1,15 @@
+# Required packages
+
+library(usethis)
+library(gitcreds)
+
+ 
+use_git_config(user.name  = "Ben Wildey",
+               user.email = "benwildey8@gmail.com")
+
+
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
+
+usethis::gh_token_help()
